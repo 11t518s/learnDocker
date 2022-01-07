f978a8ba4642d8076c57c887370b6dbe8517b39f
11t518s <11t518s20161759@gmail.com>
2022-01-07 15:52:53 +0900

    docker volume

---
+++ server.js
@@ -0,0 +1,14 @@
+const express =require('express')
+
+
+const PORT = 8080;
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello wofdfsdfsdfsdfsdfsrld')
+})
+
+app.listen(PORT)
+console.log('server is running')
